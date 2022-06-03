bcfa8d14d4ef3e5cdc9b2d6a3fa2ed2395347d38
kostiagap <98824564+kostiagap@users.noreply.github.com>
2022-06-03 23:03:00 +0400

    lol

--- app/src/main/java/com/example/text/MainActivity.java
+++ app/src/main/java/com/example/text/MainActivity.java
@@ -2,15 +2,19 @@ package com.example.text;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
 
 import java.util.Scanner;
 
 public class MainActivity extends AppCompatActivity {
+
+    public Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,5 +23,12 @@ public class MainActivity extends AppCompatActivity {
         //getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN); // убирает actionbar
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE); // делает ориентацию горизонтальной
+
+        button = (Button) findViewById(R.id.strt);
+        button.setOnClickListener((view ->{
+            Intent i = new Intent(getApplicationContext(), History.class);
+            startActivity(i);
+        }));
+
     }
 }
