6d925be5ee4983ae3974051d10c180dcc598ef73
kostiagap <98824564+kostiagap@users.noreply.github.com>
2022-06-03 21:47:05 +0400

    lol

--- app/src/main/java/com/example/text/MainActivity.java
+++ app/src/main/java/com/example/text/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.text;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 
 import java.util.Scanner;
@@ -12,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        int i=0;
+
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
     }
 }
